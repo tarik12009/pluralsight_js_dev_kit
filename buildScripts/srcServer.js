2e7338b721600bf2614d42435d3538ae97ba4b63
Tarik IRHBOLA <t.irhbola@SSM.COM>
2017-06-05 16:43:25 +0100

    Webpack integration for bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import path from 'path';
 import open from 'open';
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+import webpackDev from 'webpack-dev-middleware';
+import hmr from 'webpack-hot-middleware';
 
 const port = 3000;
 const app = express();
+const compiler = webpack(config);
+
+app.use(webpackDev(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(hmr(compiler));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
